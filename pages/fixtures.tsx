f99e2e60b7f1c8d8ea5a68c03e684ff90a91bbfe
Craig Forrest <craforrest97@gmail.com>
2021-03-20 15:53:48 +0000

    setup fixtures

---
+++ pages/fixtures.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Fixtures = () => (
+  <div>
+    <Head>
+      <title>Fixtures</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>
+      Fixtures
+    </div>
+  </div>
+)
+
+export default Fixtures

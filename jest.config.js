a13014334649f2845cd8e704b5d055aa2aa9874d
Craig Forrest <craforrest97@gmail.com>
2021-03-20 20:24:51 +0000

    add mock-next-link

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,7 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
+  setupFilesAfterEnv: [
+    './test/mock-next-link.tsx',
+  ],
 }

394ed64d550e3fd1e6eba6f3c9ce3e1769b37da1
Craig Forrest <craforrest97@gmail.com>
2021-03-21 21:30:14 +0000

    add another test for getFixture and ... to show intent

--- test/domain/fixture/api/index.test.ts
+++ test/domain/fixture/api/index.test.ts
@@ -14,7 +14,7 @@ describe('getFixtures', () => {
 });
 
 describe('getFixture', () => {
-  it('should get the fixture for a specific ID', async () => {
+  it('should get the fixture for the Watford vs Birmingham game', async () => {
     const fixture = await getFixture('593331');
 
     expect(fixture).toMatchObject({
@@ -32,6 +32,30 @@ describe('getFixture', () => {
           name: '[WATFORD_STRIKER] scores a goal',
           points: 5,
         },
+        ...
+      ])
+    })
+  });
+
+  it('should get the fixture for the Barnsley vs Sheffield Wednesday game', async () => {
+    const fixture = await getFixture('593320');
+
+    expect(fixture).toMatchObject({
+      id: '593331',
+      homeTeamName: 'Barnsley',
+      awayTeamName: 'Sheffield Wednesday',
+      events: expect.arrayContaining([
+        {
+          imageUrl: '',
+          name: '[WEDNESDAY_STRIKER] scores a goal',
+          points: 5,
+        },
+        {
+          imageUrl: '',
+          name: '[BARNSLEY_STRIKER] scores a goal',
+          points: 5,
+        },
+        ...
       ])
     })
   });

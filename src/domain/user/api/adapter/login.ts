cceecc3697674bb63195c1a7a49971fb3b277b4d
Richard Waller <richard.lh.waller@gmail.com>
2021-04-17 11:10:17 +0100

    feat: switch userIDs to usernames

--- src/domain/user/api/adapter/login.ts
+++ src/domain/user/api/adapter/login.ts
@@ -1,9 +1,21 @@
-import { v4 as uuidv4 } from 'uuid';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  animals,
+} from 'unique-names-generator';
+
 import userRepository from '../../repositories/userRepository';
 import User from '../../data/User';
 import { Login } from '../indexBackend';
 
-const createUser = (): User => ({ id: uuidv4() });
+const createUsername = () =>
+  uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    length: 2,
+    separator: '-',
+  });
+
+const createUser = (): User => ({ id: createUsername() });
 
 export const login: Login = async (userId) => {
   if (await userRepository.contains(userId)) {

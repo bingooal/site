5986c2c6b9a98ef253a3f91756ca955ff9b18880
Richard Waller <richard.lh.waller@gmail.com>
2021-06-11 18:10:57 +0100

    feat(client): on fixtures page, show competition logo

--- src/domain/fixture/components/groupedFixtureList.tsx
+++ src/domain/fixture/components/groupedFixtureList.tsx
@@ -1,10 +1,12 @@
-import FixtureList from './fixtureList';
+import Image from 'next/image';
 import FixturePreview from '../data/FixturePreview';
+import FixtureList from './fixtureList';
 
 const unique = <T,>(array: T[]): T[] => [...new Set(array)];
 
 export type GroupOfFixtures = {
   competitionName: string;
+  competitionLogo: string;
   competitionFixtures: FixturePreview[];
 };
 
@@ -16,6 +18,9 @@ export const groupByCompetition = (
   );
   return competitionNames.map((name) => ({
     competitionName: name,
+    competitionLogo: fixtures.find(
+      (fixture) => fixture.competition.name === name
+    ).competition.logo,
     competitionFixtures: fixtures.filter(
       (fixture) => fixture.competition.name === name
     ),
@@ -29,9 +34,21 @@ type Props = {
 const GroupedFixtureList: React.VFC<Props> = ({ fixtures }: Props) => (
   <>
     {groupByCompetition(fixtures).map(
-      ({ competitionName, competitionFixtures }) => (
+      ({ competitionName, competitionLogo, competitionFixtures }) => (
         <div key={competitionName}>
-          <h3 className="px-2 py-2 font-semibold text-l">{competitionName}</h3>
+          <div className="flex flex-row items-center px-2 pt-4 pb-2 space-x-2">
+            <div className="relative flex-shrink-0 w-9 h-9">
+              <Image
+                src={competitionLogo}
+                alt={competitionName}
+                layout="fill"
+                className="rounded-full"
+              />
+            </div>
+            <h3 className="px-2 py-2 font-semibold text-l">
+              {competitionName}
+            </h3>
+          </div>
           <FixtureList fixtures={competitionFixtures} />
         </div>
       )
--- test/domain/fixture/components/groupedFixtureList.test.tsx
+++ test/domain/fixture/components/groupedFixtureList.test.tsx
@@ -22,9 +22,9 @@ describe('groupByCompetition', () => {
   it.each`
     testName                                   | fixtures                                                | expected
     ${'0 fixtures'}                            | ${[]}                                                   | ${[]}
-    ${'1 fixture from 1 competition'}          | ${[fixturePreview]}                                     | ${[{ competitionName: fixturePreview.competition.name, competitionFixtures: [fixturePreview] }]}
-    ${'2 fixtures from 2 unique competitions'} | ${[fixturePreview, fixturePreviewDifferentCompetition]} | ${[{ competitionName: fixturePreview.competition.name, competitionFixtures: [fixturePreview] }, { competitionName: fixturePreviewDifferentCompetition.competition.name, competitionFixtures: [fixturePreviewDifferentCompetition] }]}
-    ${'2 fixtures from 1 unique competition'}  | ${[fixturePreview, fixturePreview]}                     | ${[{ competitionName: fixturePreview.competition.name, competitionFixtures: [fixturePreview, fixturePreview] }]}
+    ${'1 fixture from 1 competition'}          | ${[fixturePreview]}                                     | ${[{ competitionName: fixturePreview.competition.name, competitionLogo: fixturePreview.competition.logo, competitionFixtures: [fixturePreview] }]}
+    ${'2 fixtures from 2 unique competitions'} | ${[fixturePreview, fixturePreviewDifferentCompetition]} | ${[{ competitionName: fixturePreview.competition.name, competitionLogo: fixturePreview.competition.logo, competitionFixtures: [fixturePreview] }, { competitionName: fixturePreviewDifferentCompetition.competition.name, competitionLogo: fixturePreviewDifferentCompetition.competition.logo, competitionFixtures: [fixturePreviewDifferentCompetition] }]}
+    ${'2 fixtures from 1 unique competition'}  | ${[fixturePreview, fixturePreview]}                     | ${[{ competitionName: fixturePreview.competition.name, competitionLogo: fixturePreview.competition.logo, competitionFixtures: [fixturePreview, fixturePreview] }]}
   `('$testName', ({ fixtures, expected }: Table) => {
     expect(groupByCompetition(fixtures)).toEqual(expected);
   });
@@ -38,6 +38,9 @@ describe('GroupedFixtureList', () => {
 
     expect(
       screen.getByRole('heading', { name: fixturePreview.competition.name })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByAltText(fixturePreview.competition.name)
     ).toBeInTheDocument();
     expect(
       screen.getByRole('heading', {
